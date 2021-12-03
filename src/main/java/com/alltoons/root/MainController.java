88a11cc21e414b58657e648b57a89058b22a60f8
woojin97318 <woojin97318@naver.com>
2021-12-03 15:57:55 +0900

    (211203)올툰즈 프로젝트 생성 후 jsp파일 생성

---
+++ src/main/java/com/alltoons/root/MainController.java
@@ -0,0 +1,12 @@
+package com.alltoons.root;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	}
+}
