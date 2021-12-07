66801084d30f4c976a315dae7ff11ed76ae463a2
woojin97318 <woojin97318@naver.com>
2021-12-08 00:13:21 +0900

    (211208/전우진)이메일인증 관련 config패키지, dependency 추가

--- src/main/java/com/alltoons/root/member/controller/MemberController2.java
+++ src/main/java/com/alltoons/root/member/controller/MemberController2.java
@@ -2,6 +2,7 @@ package com.alltoons.root.member.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.alltoons.root.member.service.MemberService2;
@@ -11,5 +12,8 @@ import com.alltoons.root.member.service.MemberService2;
 public class MemberController2 {
 	@Autowired MemberService2 ms2;
 	
-	
+	@GetMapping("findPassword")
+	public String findPassword() {
+		return "findPassword";
+	}
 }
