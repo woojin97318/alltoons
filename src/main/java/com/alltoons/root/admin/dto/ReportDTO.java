5924c6fcd13bf7a9daacba5a7f444fb643d8609f
woojin97318 <woojin97318@naver.com>
2021-12-12 19:10:32 +0900

    (211212/전우진)관리자 페이지의 신고내용 확인 페이지 구현 완료

--- src/main/java/com/alltoons/root/admin/dto/ReportDTO.java
+++ src/main/java/com/alltoons/root/admin/dto/ReportDTO.java
@@ -1,21 +1,57 @@
-package com.alltoons.root.review.dto;
+package com.alltoons.root.admin.dto;
 
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 
-public class MyReviewDTO {
+public class ReportDTO {
+	private int reportNum;
+	private String reportUserEmail;
+	private String reportContent;
+	private String reportTime;
 	private int reviewNum;
+	private String ReviewUserEmail;
 	private String reviewContent;
 	private String reviewTime;
 	private int webtoonNum;
 	private String webtoonImage;
 	private String webtoonTitle;
 	
+	public int getReportNum() {
+		return reportNum;
+	}
+	public void setReportNum(int reportNum) {
+		this.reportNum = reportNum;
+	}
+	public String getReportUserEmail() {
+		return reportUserEmail;
+	}
+	public void setReportUserEmail(String reportUserEmail) {
+		this.reportUserEmail = reportUserEmail;
+	}
+	public String getReportContent() {
+		return reportContent;
+	}
+	public void setReportContent(String reportContent) {
+		this.reportContent = reportContent;
+	}
+	public String getReportTime() {
+		return reportTime;
+	}
+	public void setReportTime(Timestamp reportTime) {
+		SimpleDateFormat fo = new SimpleDateFormat("YYYY년MM월dd일 HH시mm분");
+		this.reportTime = fo.format(reportTime);
+	}
 	public int getReviewNum() {
 		return reviewNum;
 	}
 	public void setReviewNum(int reviewNum) {
 		this.reviewNum = reviewNum;
+	}
+	public String getReviewUserEmail() {
+		return ReviewUserEmail;
+	}
+	public void setReviewUserEmail(String reviewUserEmail) {
+		ReviewUserEmail = reviewUserEmail;
 	}
 	public String getReviewContent() {
 		return reviewContent;
