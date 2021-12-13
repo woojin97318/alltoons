aee25e3a66069c4625a455448c38e476380128d0
woojin97318 <woojin97318@naver.com>
2021-12-13 16:58:32 +0900

    (211213/전우진)member 머지(현우, 승재, 우진)

---
+++ src/main/java/com/alltoons/root/webtoon/report/mapper/ReviewReportMapper.java
@@ -0,0 +1,7 @@
+package com.alltoons.root.webtoon.report.mapper;
+
+import com.alltoons.root.webtoon.report.dto.ReviewReportDTO;
+
+public interface ReviewReportMapper {
+	public int setReport(ReviewReportDTO dto);
+}
