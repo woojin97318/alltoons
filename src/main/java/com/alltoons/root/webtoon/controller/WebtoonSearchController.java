c84ad192af2642e18bc2ec947bf2f2ac9da921aa
2ggg <larunaru0386@gmail.com>
2021-12-19 15:39:50 +0900

    (211219/이주희)헤더 디자인 수정 및 검색 오류 해결

--- src/main/java/com/alltoons/root/webtoon/controller/WebtoonSearchController.java
+++ src/main/java/com/alltoons/root/webtoon/controller/WebtoonSearchController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.alltoons.root.webtoon.service.WebtoonSearchService;
@@ -16,13 +17,14 @@ import com.alltoons.root.webtoon.service.WebtoonSearchService;
 public class WebtoonSearchController {
 	@Autowired WebtoonSearchService SearchSvc;
 	
-	@PostMapping("/webtoonSearch")//검색창
+	@PostMapping("webtoonSearch")//검색창
 	public String webtoonSearch(@RequestParam("search") String search,
 								HttpServletRequest request,
 								Model model) {
+		System.out.println("검색어: "+search);
 		model.addAttribute("search", search);
 		SearchSvc.searchList(search, model);
-		System.out.println("검색어: "+search);
+		
 		return "main/webtoonSearch";
 	}
 }
